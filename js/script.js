056b9e9b692d9ee76e146b0435a699e53f3e5a76
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-25 13:27:24 +0200

    Count var to generate num of emails added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             emails: [],  // Array per memorizzare gli indirizzi email generati
+            emailCount: 10 // Numero di email da generare
         }
     },
     methods: {
