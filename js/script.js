fee759a63046bc380d201445feec643194ff9be6
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-25 13:30:17 +0200

    Esercizio completato - Method to generate emails with inner for loop using axios added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ createApp({
         }
     },
     methods: {
-        
+        generateTenEmails() {
+            this.emails = [];  // Pulisce l'array prima di iniziare a generare le email
+            for (let i = 0; i < this.emailCount; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((resp) => {
+                        this.emails.push(resp.data.response);
+                    });
+            }
+        }
     }
 }).mount('#app');
